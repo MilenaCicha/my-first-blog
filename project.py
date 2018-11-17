cebf043c03712127b8b0e81a794bcd8104b63d79
MilenaCicha <milenaicha@yahoo.co.uk>
2018-11-17 13:58:43 +0100

    My Django Girls app

---
+++ project.py
@@ -0,0 +1,29 @@
+# print('Hello Django Girls!')
+# if 3 > 2:
+#     print('To dziala!')
+#
+# if 5 > 2:
+#     print('5 jest jednak wieksze od 2')
+# else:
+#     print('5 nie jest wieksze od 2')
+
+#
+# def hi(name):
+#  if name == 'Ola':
+#     print('Hej Ola')
+#  elif name == 'Sonja':
+#     print('Hej Sonja')
+#  else:
+#     print('Hej anonimie')
+
+# def hi(imie):
+#     print('Hej ' + imie + '!')
+# hi('Monika')
+#
+# dziewczyny = ['Rachel', 'Monica', 'Phoebe']
+# for imie in dziewczyny:
+#     hi(imie)
+#     print('Kolejna dziewczyna')
+
+for i in range(1, 6):
+    print(i)
